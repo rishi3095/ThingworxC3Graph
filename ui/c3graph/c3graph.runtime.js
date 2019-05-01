e734a18261bf2b5fca95b76e7a7301e2af421c44
rishi3095 <rishikam97@gmail.com>
2019-05-01 19:40:01 +0530

    widget working

--- ui/c3graph/c3graph.runtime.js
+++ ui/c3graph/c3graph.runtime.js
@@ -37,13 +37,17 @@ TW.Runtime.Widgets.c3graph= function () {
 	this.drawChart = function(){
 		//var id = thisWidget.mashup.rootName + "_" + thisWidget.getProperty('Id');
 		//var container = document.getElementById(id);
-		var chart = c3.generate({
-			data: {
-				columns: [
-					['data1', 30, 200, 100, 400, 150, 250],
-					['data2', 50, 20, 10, 40, 15, 25]
-				]
-			}
-		});
+		try {
+			var chart = c3.generate({
+				data: {
+					columns: [
+						['data1', 30, 200, 100, 400, 150, 250],
+						['data2', 50, 20, 10, 40, 15, 25]
+					]
+				}
+			});			
+		} catch (error) {
+			console.log(error);
+		}
 	}
 };
